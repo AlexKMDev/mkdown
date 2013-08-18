e91ff67b0b75c21c4ca1644cdd4927e7845171a3
Alexey <anakros.symbian@gmail.com>
2013-08-18 19:14:03 +0700

    - replaced tabs with spaces  - replaced string concatenation with string interpolation  - many other changes

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ mkdown
 music downloader for vk.com
 
 1. clone repository or download mkdown.rb
-2. $ chmod +x mkdown.rb
-3. get your token by visiting this link: https://oauth.vk.com/authorize?client_id=3718612&scope=audio,offline&redirect_uri=http://oauth.vk.com/blank.html&display=wap&response_type=token
-4. start mkdown with your token: $ ./mkdown.rb 12345667890
+2. ```$ chmod +x mkdown.rb```
+3. get your token by visiting [https://oauth.vk.com/authorize?client_id=3718612&scope=audio&redirect_uri=http://oauth.vk.com/blank.html&display=wap&response_type=token](this) link.
+4. start mkdown with your token: ```$ ./mkdown.rb 12345667890```
--- mkdown.rb
+++ mkdown.rb
@@ -1,71 +1,70 @@
 #!/usr/bin/ruby
 # encoding: utf-8
-#
-# please get new token by visiting link below
-# https://oauth.vk.com/authorize?client_id=3718612&scope=audio,offline&redirect_uri=http://oauth.vk.com/blank.html&display=wap&response_type=token
+
 require 'net/http'
 require 'json'
 require 'cgi'
 
 def show_help
-	puts <<-eos
-	-h - show this message
-	<token> - download all your songs, access_token required
-	eos
+  puts "Usage: ./mkdown.rb <access_token>"
 end
 
 def get_audio_list(token)
-	puts 'downloading audio list...'
+  puts 'downloading audio list...'
 
-	uri = URI('https://api.vk.com/method/audio.get?access_token=' + token)
-	response = Net::HTTP.get(uri)
+  uri = URI('https://api.vk.com/method/audio.get?access_token=' + token)
+  response = Net::HTTP.get uri
 
-	JSON.parse response
+  JSON.parse response
 end
 
-def make_safe_str(string)
-	CGI.unescapeHTML(string).gsub(/\/|\\/, '\\')
+def safe_str(string)
+  CGI.unescapeHTML(string).gsub(/\//, '\\')
 end
 
 def save_file(filename, file_uri)
-	music_dir = Dir.pwd + '/music/'
-	Dir.mkdir(music_dir, 0755) unless Dir.exist? music_dir
+  music_dir = Dir.pwd + '/music/'
+  Dir.mkdir music_dir, 0755 unless Dir.exist? music_dir
 
-	if File.file?(music_dir + filename)
-		puts filename + ' already exists.'
-	else
-		puts 'downloading ' + filename + '...'
+  if File.file? music_dir + filename
+    puts "#{filename} already exists."
+  else
+    puts "downloading #{filename}..."
 
-		File.open(music_dir + filename, 'wb') do |f|
-			response = Net::HTTP.get(file_uri)
-			f.write(response)
-		end
-	end
+    File.open music_dir + filename, 'wb' do |f|
+      response = Net::HTTP.get file_uri
+      f.write response
+    end
+  end
 end
 
 def prepare_songs(audio_list)
-	queue_count = audio_list.count.to_s
-	puts queue_count + ' songs in queue.'
+  queue_count = audio_list.count.to_s
+  puts "#{queue_count} songs in queue."
 
-	audio_list.each_index do |song|
-		print "[#{ song + 1 }/#{ queue_count }] "
+  audio_list.each_index do |song|
+    print "[#{ song + 1 }/#{ queue_count }] "
 
-		song = audio_list[song]
-		filename = make_safe_str("#{ song['artist'] } - #{ song['title'] }.mp3")
-		file_uri = URI.parse(song['url'])
+    song = audio_list[song]
+    filename = safe_str "#{ song['artist'] } - #{ song['title'] }.mp3"
+    file_uri = URI.parse song['url']
 
-		save_file(filename, file_uri)
-	end
+    save_file(filename, file_uri)
+  end
 end
 
-if ARGV.empty? || ARGV[0] == '-h'
-	show_help
-elsif !ARGV[0].empty?
-	audio_list = get_audio_list(ARGV[0])
+if !ARGV.first || ARGV.first == '-h'
+  show_help
+else
+  print 'Music will be downloaded in new "music" directory in current directory. Ok? (y/n): '
+  answer = STDIN.gets.chomp()
+  exit if answer != 'y'
 
-	if audio_list['error'].nil?
-		prepare_songs(audio_list['response'])
-	else
-		puts audio_list['error']['error_msg']
-	end
+  audio_list = get_audio_list(ARGV.first)
+
+  if audio_list['error'].nil?
+    prepare_songs(audio_list['response'])
+  else
+    puts audio_list['error']['error_msg']
+  end
 end
